2671c9484350bb24e7332f717f6a6a6ba2a12155
Antonio de la Rosa <santiagodelarosaantonio@gmail.com>
2023-01-08 13:06:26 +0100

    Menu en la MainActivity añadido - Menu con todas las opciones añadido - Eliminado los iconos de menu_details_workplace y menu_workplaces para mas claridad, solo se muestra el texto

--- app/src/main/java/com/svalero/workhub/MainActivity.java
+++ app/src/main/java/com/svalero/workhub/MainActivity.java
@@ -1,16 +1,21 @@
 package com.svalero.workhub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -74,10 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
         bAdministration = findViewById(R.id.administration);
         bAdministration.setOnClickListener(view -> {
-            //Intent intent = new Intent(this, TestMap.class);
-            //intent.putExtra("userID", userID);
-            //intent.putExtra("username", username);
-            //startActivity(intent);
+            Toast.makeText(this, "TODO. Sorry.", Toast.LENGTH_LONG).show();
         });
     }
 
@@ -89,5 +91,44 @@ public class MainActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},225);
             }
         }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if(item.getItemId() == R.id.menuWorkplaces){
+            Intent intent = new Intent(this, ListWorkplaces.class);
+            intent.putExtra("userID", userID);
+            intent.putExtra("username", username);
+            startActivity(intent);
+            return true;
+        } else if(item.getItemId() == R.id.menuReserves){
+            Intent intent = new Intent(this, ListReserves.class);
+            intent.putExtra("userID", userID);
+            intent.putExtra("username", username);
+            startActivity(intent);
+        } else if(item.getItemId() == R.id.menuUser){
+            Intent intent = new Intent(this, DetailUser.class);
+            intent.putExtra("userID", userID);
+            intent.putExtra("username", username);
+            startActivity(intent);
+        } else if(item.getItemId() == R.id.menuLogout){
+            AlertDialog.Builder deleteDialog = new AlertDialog.Builder(this);
+            deleteDialog.setMessage(R.string.confirmationMessage).setTitle(R.string.logoutMessage)
+                    .setPositiveButton(R.string.confirmationYes, (dialog, id) -> {
+                        Intent intent = new Intent(this, Login.class);
+                        startActivity(intent);
+                    }).setNegativeButton(R.string.confirmationNo, (dialog, id) -> {
+                        dialog.dismiss();
+                    });
+            AlertDialog dialog = deleteDialog.create();
+            dialog.show();
+        }else if(item.getItemId() == R.id.menuSettings){
+            Toast.makeText(this, "TODO. Sorry.", Toast.LENGTH_LONG).show();
+        }
+        return false;
     }
 }
